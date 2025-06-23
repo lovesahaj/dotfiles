9449afc5c1c12b4333ce78a85654d0d08802f036
lovesahaj <lovesahaj1225@gmail.com>
2025-06-24 02:02:37 +0530

    adding changes to wezterm and nvim and kitty

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -425,15 +425,14 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = { 'isort', 'ruff' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
       formatters = {
-        black = {
-          prepend_args = { '--line-length', '80', '--fast' },
-        },
+        iron = {},
+        ruff = {},
       },
     },
     init = function()
